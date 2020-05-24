eaaab2e2320a5ee7feaea95effc18bbc9e84355a
Jakub Hodyl <jakubhodyl@gmail.com>
2020-05-24 02:11:07 +0200

    Save fetched data to state

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
 import { fetchCurrentWeatherByID } from '../weather-service/weather-service';
 import Search from '../data-wrapper/search/Search';
+import { isObjEmpty } from '../utils/utils';
 
 const Dashboard = () => {
   const [weatherData, setWeatherData] = useState({});
 
   const fetchWeatherData = (id) => {
-    fetchCurrentWeatherByID(id);
+    fetchCurrentWeatherByID(id, setWeatherData);
   };
+
+  console.log(weatherData);
 
   return (
     <div className="main-container">
       <div className="data-wrapper-container">
-        <Search fetchWeatherData={fetchWeatherData} />
+        {isObjEmpty(weatherData) ? (
+          <Search fetchWeatherData={fetchWeatherData} />
+        ) : (
+          <p>Henlo</p>
+        )}
       </div>
     </div>
   );
